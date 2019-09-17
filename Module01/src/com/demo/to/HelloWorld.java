b34e3a192d43ff91191356eece07ff4c9eb15fde
Luckyi998 <506852455@qq.com>
2019-09-17 19:04:39 +0800

    Initial commit

---
+++ Module01/src/com/demo/to/HelloWorld.java
@@ -0,0 +1,35 @@
+package com.demo.to;
+
+import java.util.ArrayList;
+
+public class HelloWorld {
+
+    public static void main (String[] args){
+        System.out.println("HelloWorld!!!!");
+
+        ArrayList list = new ArrayList();
+    }
+
+
+    public void Test(){
+        String order = "order";
+        ArrayList<Object> objects = new ArrayList<>();
+        int i = 12;
+
+        /**
+         * sdfds
+         */
+         private Integer sdfds ;
+          /**
+           * s
+           */
+           private String s ;
+
+
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
---
+++ src/com/demo/to/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.demo.to;
+
+public class HelloWorld {
+
+    public static void main (String[] args){
+        System.out.println("HelloWorld!!");
+    }
+
+}
